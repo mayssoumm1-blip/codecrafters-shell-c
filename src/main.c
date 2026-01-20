4edc6dc8c8bd1ec0ab44b6221ed36d7b17424188
mayssoum <mayssoumm1@gmail.com>
2026-01-20 12:56:06 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  // Captures the user's command in the "command" variable
+  char command[1024];
+  fgets(command,sizeof(command), stdin);
+  // Remove the trailing newline
+  command[strcspn(command, "\n")] = '\0';
+  // Prints the "<command>: command not found" message
+  printf("%s: command not found\n", command);
+
 
   return 0;
 }
